d864763a30e80aebb52045b8589e7b1b3b901692
Nirmal Garg <nirmalgarg2006@gmail.com>
2020-06-22 23:51:58 +0530

    lightning component lwc2

---
+++ SampleProject2/force-app/main/default/lwc/lwc2/lwc2.js
@@ -0,0 +1,38 @@
+import { LightningElement, wire, track  } from 'lwc';
+import lstAccounts from '@salesforce/apex/AccountController.getAccountList';
+export default class Lwc2 extends LightningElement {
+    @track accounts;
+    @track personname;
+    personname = 'Nirmal';
+    @wire(lstAccounts)
+    m1({data}){
+        if(data)
+        {
+            this.accounts = data;
+        }
+    }
+
+    changepersonname(event){
+        this.personname = event.target.value;
+    }
+
+    firstname = '';
+    lastname = '';
+
+    handlechange(event)
+    {
+        const field = event.target.name;
+        if(field === 'firstname'){
+            this.firstname = event.target.value;
+        }
+        else if(field === 'lastname')
+        {
+            this.lastname = event.target.value;
+        }
+    }
+
+    get uppercasefullname()
+    {
+        return `${this.firstname} ${this.lastname}`.toUpperCase();
+    }
+} 
